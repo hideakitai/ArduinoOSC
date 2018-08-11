08b9386c620f6e349fc0584efef7b8db001c53fc
Hideaki Tai <hideaki.tai@gmail.com>
2018-08-11 15:42:52 +0900

    fix build error for teensy (dirty)

--- ArduinoOSC.h
+++ ArduinoOSC.h
@@ -63,6 +63,13 @@
     #include "ArduinoOSC/OSCMessage.h"
     #include "ArduinoOSC/OSCClient.h"
     #include "ArduinoOSC/OSCServer.h"
+    #ifdef TEENSYDUINO // dirty
+        namespace std
+        {
+            void __throw_bad_alloc() { Serial.println("Unable to allocate memory"); }
+            // void __throw_length_error(char const* e) { Serial.print("Length Error :"); Serial.println(e); }
+        }
+    #endif
 #else
     #error UNSUPPORTED PLATFORM
 #endif
